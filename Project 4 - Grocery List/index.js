ee904da1f520cec6ae3c137ef117048fd298cf64
awareermine <aramie.ewen@gmail.com>
2022-10-12 19:37:49 -0400

    Vanilla js drag and drop

--- Project 4 - Grocery List/index.js
+++ Project 4 - Grocery List/index.js
@@ -1,7 +1,52 @@
-import { htmlToElement, shuffle } from './utils.js';
+import { htmlToElement } from './utils.js';
+
+function onLiDrag(event) {
+    event.dataTransfer.setData("dragged-id", event.target.id);
+}
+
+function onLiDrop(event) {
+    event.preventDefault();
+    const dragged = document.getElementById(event.dataTransfer.getData("dragged-id"));
+    if (event.currentTarget.offsetTop < dragged.offsetTop) {
+        event.currentTarget.parentNode.insertBefore(dragged, event.currentTarget);
+    } else {
+        event.currentTarget.after(dragged);
+    }
+}
+
+function deleteElement(event) {
+    event.target.parentNode.parentNode.remove();
+}
+
+function addNew(event) {
+    event.preventDefault();
+    const listInput = event.target.elements.listInput;
+
+    if (!listInput.value.trim()) return; // Don't add empty list items
+
+    const listItems = document.getElementById("list-items");
+    listItems.appendChild(htmlToElement(`
+        <li 
+            draggable="true" 
+            ondragstart="window.onLiDrag(event)"
+            ondragover="event.preventDefault()"
+            ondrop="window.onLiDrop(event)"
+            id=${Date.now()}
+        >
+            <div class="items">
+                <p>${listInput.value}</p>
+                <button class="delete-btn" onclick="window.deleteElement(event)">delete</delete>
+            </div>
+        </li>
+    `))
+    listInput.value = "";
+}
 
 function onPageLoad(evt) {
-  
+    window.addNew = addNew;
+    window.deleteElement = deleteElement;
+    window.onLiDrag = onLiDrag;
+    window.onLiDrop = onLiDrop;
 }
 
 document.addEventListener("DOMContentLoaded", onPageLoad)
--- Project 4 - Grocery List/utils.js
+++ Project 4 - Grocery List/utils.js
@@ -5,15 +5,3 @@ export function htmlToElement(html) {
     
     return template.content.firstChild;
 }
-
-export function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    
-    return array;
-}
