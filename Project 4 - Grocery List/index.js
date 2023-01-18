5ba201c95b5e4a6c7a88cd48afc313433ab08cfd
Aramie Ewen <68764600+AwareErmine@users.noreply.github.com>
2023-01-18 09:04:59 -0500

    update project 4

--- Project 4 - Grocery List/index.js
+++ Project 4 - Grocery List/index.js
@@ -1,10 +1,7 @@
 import { htmlToElement } from './utils.js';
 
-const deletedHistory = [];
-
-function onLiDrag(event) {
-    event.dataTransfer.setData("dragged-id", event.target.id);
-}
+var deletedHistory = [];
+const audio = new Audio("http://freesoundeffect.net/sites/default/files/paper-crumple-01-sound-effect-45574952.mp3");
 
 function onLiDrop(event) {
     event.preventDefault();
@@ -17,8 +14,10 @@ function onLiDrop(event) {
 }
 
 function deleteElement(event) {
-    deletedHistory.push(event.target.parentNode.parentNode);
+    audio.play();
+    deletedHistory.push(event.target.parentNode.innerHTML); // the list item whole
     event.target.parentNode.parentNode.remove();
+    // console.log(deletedHistory);
 }
 
 function addNew(event) {
@@ -46,17 +45,57 @@ function addNew(event) {
 }
 
 function onPageLoad(evt) {
+    const isUser = localStorage.getItem("isUser");
+    if (!isUser) {
+        document.getElementById("info-modal").classList.remove("hidden"); 
+        localStorage.setItem("listItems", '');
+        localStorage.setItem("deletedHistory", ''); 
+    } else {
+        deletedHistory = JSON.parse(localStorage.getItem("deletedHistory"));
+        // console.log("deleted history - localstorage", JSON.parse(localStorage.getItem("deletedHistory")));
+        // console.log("deleted history - now", deletedHistory);
+        document.getElementById("list-items").innerHTML = localStorage.getItem("listItems");
+    }
+    localStorage.setItem("isUser", true)
+    // localStorage.clear() // for testing
+
     window.addNew = addNew;
     window.deleteElement = deleteElement;
-    window.onLiDrag = onLiDrag;
+    window.onLiDrag = (evt) => evt.dataTransfer.setData("dragged-id", evt.target.id);
     window.onLiDrop = onLiDrop;
+}
+
+function beforeUnload(evt) {
+    evt.preventDefault()
+
+    // set list items 
+    localStorage.setItem("listItems", document.getElementById("list-items").innerHTML);
+    // set deleted history 
+    localStorage.setItem("deletedHistory", JSON.stringify(deletedHistory));
 }
 
 function undoDelete(event) {
     if (event.ctrlKey && event.key === 'z' && deletedHistory.length) {
-        document.getElementById("list-items").appendChild(deletedHistory.pop());
+        const elem = deletedHistory.pop();
+
+        const listItems = document.getElementById("list-items");
+        listItems.appendChild(htmlToElement(`
+            <li 
+                draggable="true" 
+                ondragstart="window.onLiDrag(event)"
+                ondragover="event.preventDefault()"
+                ondrop="window.onLiDrop(event)"
+                id=${Date.now()}
+            >
+                <div class="items">
+                    ${elem}
+                </div>
+            </li>
+        `))
     }
 }
 
 document.addEventListener('keydown', undoDelete);
 document.addEventListener("DOMContentLoaded", onPageLoad);
+// document.addEventListener("beforeunload", beforeUnload);
+window.onbeforeunload = beforeUnload;
